717a5cc814ea9b19cea2e4dfe0914e285a1da937
Алексей <shimaruan747@gmail.com>
2018-05-05 23:49:52 +0300

    внесены правки в соответствии с требованиями наставника

--- source/js/script.js
+++ source/js/script.js
@@ -1,18 +1,22 @@
 var menu= document.querySelector("nav");
 var modalMenu= document.querySelector(".main-navigation__list");
-var menuButtonOpen= document.querySelector(".main-navigation__burger-button");
-var menuButtonClose= document.querySelector(".main-navigation__close-button");
+var menuButtonBurger= document.querySelector(".main-navigation__button-burger");
+var menuButtonOpen= document.querySelector(".main-navigation__button-burger");
+var menuButtonClose= document.querySelector(".main-navigation__button-close");
+
 
 modalMenu.classList.remove("main-navigation__list-opened");
+menuButtonBurger.classList.add("main-navigation__button-show");
+
 
 menuButtonOpen.addEventListener("click", function(evt) {
   evt.preventDefault();
   modalMenu.classList.add("main-navigation__list-opened");
-  menuButtonClose.classList.add("main-navigation__close-button-show");
+  menuButtonClose.classList.add("main-navigation__button-show");
 });
 
 menuButtonClose.addEventListener("click", function(evt) {
   evt.preventDefault();
-  menuButtonClose.classList.remove("main-navigation__close-button-show");
+  menuButtonClose.classList.remove("main-navigation__button-show");
   modalMenu.classList.remove("main-navigation__list-opened");
 });
