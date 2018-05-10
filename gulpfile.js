3564199a18afcb5c9dcc3c66e4995254c51d2047
firefoxic <firefoxic.arts@gmail.com>
2018-05-11 00:43:02 +0300

    Remove excess copying from source to build

--- gulpfile.js
+++ gulpfile.js
@@ -79,9 +79,7 @@ gulp.task("clean", function () {
 gulp.task("copy", function () {
   return gulp.src([
       "source/fonts/**/*.{woff,woff2}",
-      "source/img/**",
       "source/js/**",
-      "source/css/**",
       "source/*.html"
     ], {
       base: "source"
