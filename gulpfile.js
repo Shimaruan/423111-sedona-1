72b3655d181d39b8b6704f6b308ecfde9a803d72
Алексей <shimaruan747@gmail.com>
2018-05-13 20:21:24 +0300

    добавлены модальные окна

--- gulpfile.js
+++ gulpfile.js
@@ -44,7 +44,7 @@ gulp.task("style", function() {
 gulp.task("js-min", function() {
   return  gulp.src("source/js/*.js")
     .pipe(uglify())
-    .pipe(rename({suffix: '-min'}))
+    .pipe(rename({suffix: "-min"}))
     .pipe(gulp.dest("build/js"));
  });
 
